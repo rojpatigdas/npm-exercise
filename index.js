a174b15b9ca65b1e7243271078e487430003fdca
Rojenn Clyde Patigdas <patigdasr@gmail.com>
2025-07-17 07:25:54 +0800

    random silly name and superhero name generator

---
+++ index.js
@@ -0,0 +1,11 @@
+import generateName from "sillyname"
+import superheroes from "superheroes"
+import {randomSuperhero} from 'superheroes'
+
+
+var sillyName = generateName()
+var heroName = randomSuperhero()
+
+
+console.log(`My name is ${sillyName}`)
+console.log(`My superhero name is ${heroName}!`)
